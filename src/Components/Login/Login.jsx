08d40a7f1f4e5d4cc0d5ad3f4d47b50d76030b53
zubayir mukhammadjonov <shadowmonarch1107@gmail.com>
2025-05-20 15:05:44 +0500

    login and register components are done

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,27 +1,60 @@
-import React, { useEffect, useState } from 'react'
-import './login.css'
-import './media.css'
-import movieLogo from '../../assets/Movie.png'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import "./login.css";
+import "./media.css";
+import movieLogo from "../../assets/Movie.png";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
 
-  return (
-    <div className='login'>
-        <div className="login_container">
-            <img src={movieLogo} alt="Error" />
+  const [loginmail, setLoginMail] = useState("");
+  const [logincode, setLoginCode] = useState("");
 
-            <form className='login_form'>
-              <h1 className="login_title">Login</h1>
-              <input type="text" className="input_email" placeholder='Email address' />
-              <input type="password" className="input_pass" placeholder='Password' />
-              <button className="login_account"><Link to="/movie">Login to your account</Link></button>
-              <pre className="login_go">Don’t have an account?  <Link to="/sign" > Sign Up</Link></pre>
-            </form>
-        </div>
+  const navigate = useNavigate();
+
+  const loginEmail = localStorage.getItem("email");
+  const loginPass = localStorage.getItem("password");
+
+  const handleLogin = () => {
+    if(loginmail === loginEmail || logincode === loginPass) {
+      navigate("/movie");
+    } else {
+      alert("Invalid email or password");
+    }
+
+
+    
+  }
+
+  return (
+    <div className="login">
+      <div className="login_container">
+        <img src={movieLogo} alt="Error" />
+
+        <form className="login_form">
+          <h1 className="login_title">Login</h1>
+          <input
+            type="text"
+            className="input_email"
+            placeholder="Email address"
+            onChange={(e) => setLoginMail(e.target.value)}
+          />
+          <input
+            type="password"
+            className="input_pass"
+            placeholder="Password"
+            onChange={(e) => setLoginCode(e.target.value)}
+          />
+          <button className="login_account"onClick={(e) => handleLogin()}>
+            <a>Login to your account</a>
+          </button>
+          <pre className="login_go">
+            Don’t have an account? <Link to="/sign"> Sign Up</Link>
+          </pre>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/Components/Sign/Sign.jsx
+++ src/Components/Sign/Sign.jsx
@@ -1,19 +1,65 @@
-import './sign.css'
+import "./sign.css";
+import "./media.css";
+import movieLogo from "../../assets/Movie.png";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Sign() {
-  
 
+  const [signEmail, setSignEmail] = useState("");
+  const [signPass, setSignPass] = useState("");
+  const [signPass2, setSignPass2] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSignClick = () => {
+
+    localStorage.setItem("email", signEmail);
+    localStorage.setItem("password", signPass);
+    localStorage.setItem("password2", signPass2);
+
+    navigate("/movie");
+  }
 
   return (
     <div>
+      <div className="register">
+        <div className="regis_container">
+          <img src={movieLogo} alt="Error" />
 
-      
+          <form className="regis_form">
+            <h1 className="regis_title">Sign Up</h1>
+            <input
+              type="text"
+              className="input_email"
+              placeholder="Email address"
+              onChange={(e) => setSignEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              className="input_pass"
+              placeholder="Password"
+              onChange={(e) => setSignPass(e.target.value)}
+            />
 
+            <input
+              type="password"
+              className="input_pass2"
+              placeholder="Password"
+              onChange={(e) => setSignPass2(e.target.value)}
+            />
 
+            <button className="regis_account" onClick={(e) => handleSignClick()}>
+              <Link to="/movie">Create an account</Link>
+            </button>
+            <pre className="regis_go">
+              Already have an account? <Link to="/"> Login</Link>
+            </pre>
+          </form>
+        </div>
+      </div>
     </div>
-
-
-  )
+  );
 }
 
-export default Sign
+export default Sign;
